add8afeccffc13ed8166ea0b1b96d6b7114a45ae
JTF4 <issues@jtf4.com>
2021-02-03 01:08:53 -0500

    Reordering index.js

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+// Config
+// MQTT Authentication
+var MQTTuser = 'mqtt';
+var MQTTpass = 'mqtt';
+
+// MQTT Broker Server Connection
+var MQTTtopic = 'audio/audia/command';
+var MQTTbroker = '10.2.10.33';
+var MQTTport = 1883; // Default 1883
+
+// DSP Connection
+var DSPip = '10.2.10.200';
+var DSPport = 23; // Default 23 (Should never have to change this)
+// End Config
+
+
 var net = require('net');
 
 var mqtt = require('mqtt'), url = require('url');
 // Parse
-var mqtt_url = url.parse(process.env.CLOUDAMQP_MQTT_URL || 'mqtt://10.2.10.33:1883');
+var mqtt_url = url.parse(process.env.CLOUDAMQP_MQTT_URL || 'mqtt://' + MQTTbroker + ':' + MQTTport);
 var auth = (mqtt_url.auth || ':').split(':');
 var url = "mqtt://" + mqtt_url.host;
 //username: auth[0] + ":" + auth[0] if you are on a shared instance
 var options = {
   port: mqtt_url.port,
   clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-  username: "mqtt",
-  password: "mqtt",
+  username: MQTTuser,
+  password: MQTTpass,
 };
 
 // Create a client connection
@@ -19,14 +35,14 @@ var client = mqtt.connect(url, options);
 client.on('connect', function() { // When connected
 
   // subscribe to a topic
-  client.subscribe('audio/audia/command', function() {
+  client.subscribe(MQTTtopic, function() {
     // when a message arrives, do something with it
     client.on('message', function(topic, message, packet) {
       console.log("Received '" + message + "' on '" + topic + "'");
       
         var client = new net.Socket();
 
-        client.connect(23, '10.2.10.200', function() {
+        client.connect(DSPport, DSPip, function() {
         console.log(`sending to server: a.random.test`);
         client.write(message + `\n`);     
         client.end();                 
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Audia-Nexia-MQTT-Translation-Server
+This server translates MQTT commands to TCP commands for the Biamp Audia and Nexia family of DSPs
+
+# Configuration
+## Translation Server
+* Insert the IP of your Audia/Nexia device in the host field.
+* Insert the MQTT topic to subscribe to.
+* Run index.js
+
+## Home Assistant
+* If you do not aleady have an MQTT broker, install the mosquito MQTT broker add-on from supervisor.
+* Setup the MQTT integration with the IP of your broker server.
+* Whenever you want to send the Audia/Nexia DSP a command, call the `mqtt.publish` service.
+    * Go to https://support.biamp.com/Audia-Nexia/Control/Audia-Nexia_command_string_calculator to create your Audia/Nexia command string.
+    * In the service data section insert: 
+    
+        ```
+         topic: <MQTT Topic
+         payload: <Audia/Nexia Command String>
+        ```
+
+
+
+
+
+
+## Future Updates
+* [ ] Add feedback from the Audia/Nexia DSP as a MQTT sensor in HASSIO
